85e1d938ed0ecbca33f423f0edf16e4563b21812
Serg Mark <sgcont@gmail.com>
2023-02-09 16:47:34 +0300

    Added bootstrap markup for registration page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Register Page</div>
+  <div>
+    <div class="auth-page">
+      <div class="container page">
+        <div class="row">
+          <div class="col-md-6 offset-md-3 col-xs-12">
+            <h1 class="text-xs-center">Sign Up</h1>
+            <p class="text-xs-center">
+              <router-link :to="{name: 'login'}">Need an account?</router-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
