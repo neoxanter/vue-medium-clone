29058d38350a297d042c5eaf5c88bc24c4f9e3f9
Serg Mark <sgcont@gmail.com>
2023-02-10 18:48:05 +0300

    Added mutations Succes and Failure

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -8,17 +8,26 @@ const mutations = {
   registerStart(state) {
     state.isSubmitting = true
   },
+  registerSuccess(state) {
+    state.isSubmitting = false
+  },
+  registerFailure(state) {
+    state.isSubmitting = false
+  },
 }
 
 const actions = {
   register(context, credentials) {
     return new Promise(() => {
+      context.commit('registerStart')
       authApi
         .register(credentials)
         .then((response) => {
           console.log('response', response)
+          context.commit('registerSuccess', response.data.user)
         })
         .catch((result) => {
+          context.commit('registerFailure')
           console.log('result errors', result)
         })
     })
