c04e729911aef86006fd2c4cd2572e5d0ba099d2
Serg Mark <sgcont@gmail.com>
2023-02-15 19:57:54 +0300

    Created Login.vue component

---
+++ src/views/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="auth-page">
+      <div class="container page">
+        <div class="row">
+          <div class="col-md-6 offset-md-3 col-xs-12">
+            <h1 class="text-xs-center">Sign In</h1>
+            <p class="text-xs-center">
+              <router-link :to="{name: 'register'}"
+                >Need an account?</router-link
+              >
+            </p>
+            <vmc-validation-errors
+              v-if="validationErrors"
+              :validation-errors="validationErrors"
+            />
+            <form @submit.prevent="onSubmit">
+              <fieldset class="form-group">
+                <input
+                  type="password"
+                  class="form-control form-control-lg"
+                  placeholder="Password"
+                  v-model="password"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  placeholder="Email"
+                  v-model="email"
+                />
+              </fieldset>
+
+              <button
+                class="
+                  btn btn-lg btn-primary
+                  pull-xs-right
+                  :disabled='isSubmitting'
+                "
+              >
+                Sign In
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VmcValidationErrors from '@/components/ValidationErrors'
+import {actionTypes} from '@/store/modules/auth'
+
+export default {
+  name: 'VmcLogin',
+  components: {
+    VmcValidationErrors,
+  },
+  data() {
+    return {
+      password: '',
+      email: '',
+    }
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    },
+    validationErrors() {
+      return this.$store.state.auth.validationErrors
+    },
+  },
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch(actionTypes.login, {
+          password: this.password,
+          email: this.email,
+        }) // eslint-disable-next-line
+        .then(() => {
+          this.$router.push({name: 'home'})
+        })
+    },
+  },
+}
+</script>
