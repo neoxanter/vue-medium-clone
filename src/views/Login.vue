57bffea74370b199ef98b6e04e270b4dbe656f0c
Serg Mark <sgcont@gmail.com>
2023-02-15 20:20:25 +0300

    Rewrote computed via the mapState method in the Login and Register components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -50,6 +50,7 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import VmcValidationErrors from '@/components/ValidationErrors'
 import {actionTypes} from '@/store/modules/auth'
 
@@ -65,12 +66,17 @@ export default {
     }
   },
   computed: {
-    isSubmitting() {
-      return this.$store.state.auth.isSubmitting
-    },
-    validationErrors() {
-      return this.$store.state.auth.validationErrors
-    },
+    ...mapState({
+      isSubmitting: (state) => state.auth.isSubmitting,
+      validationErrors: (state) => state.auth.validationErrors,
+    }),
+
+    // isSubmitting() {
+    //   return this.$store.state.auth.isSubmitting
+    // },
+    // validationErrors() {
+    //   return this.$store.state.auth.validationErrors
+    // },
   },
   methods: {
     onSubmit() {
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -56,6 +56,7 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import VmcValidationErrors from '@/components/ValidationErrors'
 import {actionTypes} from '@/store/modules/auth'
 
@@ -72,12 +73,10 @@ export default {
     }
   },
   computed: {
-    isSubmitting() {
-      return this.$store.state.auth.isSubmitting
-    },
-    validationErrors() {
-      return this.$store.state.auth.validationErrors
-    },
+    ...mapState({
+      isSubmitting: (state) => state.auth.isSubmitting,
+      validationErrors: (state) => state.auth.validationErrors,
+    }),
   },
   methods: {
     onSubmit() {
