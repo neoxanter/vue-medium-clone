8cf51875b86bf9298f57dc19137ebd19bdd90b4e
Serg Mark <sgcont@gmail.com>
2023-02-17 23:38:34 +0300

    Getting the necessary data from vuex - user status and current user

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Top Bar</div>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <router-link class="navbar-link" :to="{name: 'home'}"
+        >MediumClone</router-link
+      >
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   name: 'VmcTopBar',
+  computed: {
+    ...mapState({
+      currentUser: (state) => state.auth.currentUser,
+      isLoggedIn: (state) => state.auth.isLoggedIn,
+    }),
+  },
 }
 </script>
