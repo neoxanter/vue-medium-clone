b913b6ed4858eea30b4d20ab94ad3b95dc9ad448
Serg Mark <sgcont@gmail.com>
2023-02-21 00:08:50 +0300

    Setting template with isAnonymous - now links for registered users are not visible to unregistered

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -43,7 +43,7 @@
             </router-link>
           </li>
         </template>
-        <template v-if="!isLoggedIn">
+        <template v-if="isAnonymous">
           <li class="nav-item">
             <router-link
               class="nav-link"
@@ -76,10 +76,16 @@ export default {
   computed: {
     ...mapState({
       // currentUser: (state) => state.auth.currentUser,
-      isLoggedIn: (state) => state.auth.isLoggedIn,
+      // isLoggedIn: (state) => state.auth.isLoggedIn,
     }),
     currentUser() {
       return this.$store.getters[getterTypes.currentUser]
+    },
+    isLoggedIn() {
+      return this.$store.getters[getterTypes.isLoggedIn]
+    },
+    isAnonymous() {
+      return this.$store.getters[getterTypes.isAnonymous]
     },
   },
 }
