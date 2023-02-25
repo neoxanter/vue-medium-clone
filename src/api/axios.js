337b2a5bc766a3e352151ebf3b5bc3133232fab4
Serg Mark <sgcont@gmail.com>
2023-02-25 22:19:37 +0300

    Added an interceptor to attach a token with every request

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://conduit.productionready.io/api'
+axios.interceptors.request.use(config => {
+    return config
+})
 
 export default axios
