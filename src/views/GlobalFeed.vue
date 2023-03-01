f518dd09116e61c8f1b97c6a2e1a7cbe59dfb769
Serg Mark <sgcont@gmail.com>
2023-03-01 22:15:50 +0300

    Created markup in GlobalFeed

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>GlobalFeed</div>
+  <div class="home-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <vcm-feed />
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import VmcFeed from '@/components/Feed'
 export default {
   name: 'VmcGlobalFeed',
+  components: {
+    VmcFeed,
+  },
 }
 </script>
