d587332a8edb1324e297f11478a592e2045f330d
Serg Mark <sgcont@gmail.com>
2023-03-04 20:45:56 +0300

    Added mutations to the feed module

--- src/store/modules/feed.js
+++ src/store/modules/feed.js
@@ -5,11 +5,25 @@ const state = {
 }
 
 export const mutationTypes = {
-    getFeedStart: '[feed] Get feed start'
-    getFeedSuccess: '[feed] Get feed success'
+    getFeedStart: '[feed] Get feed start',
+    getFeedSuccess: '[feed] Get feed success',
     getFeedFailure: '[feed] Get feed failure'
 }
 
 export const actionTypes = {
     getFeed: '[feed] Get feed'
+}
+
+const mutations = {
+    [mutationTypes.getFeedStart](state) {
+        state.isLoading = true,
+            state.data = null
+    },
+    [mutationTypes.getFeedSuccess](state, payload) {
+        state.isLoading = false,
+            state.data = payload
+    },
+    [mutationTypes.getFeedFailure](state) {
+        state.isLoading = false,
+    }
 }
