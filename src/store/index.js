d55d8807714e8d2ce16ad84151c14bb816f6f450
Serg Mark <sgcont@gmail.com>
2023-03-06 21:34:55 +0300

    Register the feed module in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from '@/store/modules/auth'
+import feed from '@/store/modules/feed'
 
 Vue.use(Vuex)
 
@@ -12,5 +13,6 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     auth,
+    feed
   },
 })
